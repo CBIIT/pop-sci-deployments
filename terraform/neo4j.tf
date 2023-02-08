a12b44a33c7eeebb6bd6d3227122a1a3f38e9de9
Ngu <nguca@nci-02277953-ml.mynetworksettings.com>
2023-02-08 10:25:28 -0500

    updated neo4j.tf

--- terraform/neo4j.tf
+++ terraform/neo4j.tf
@@ -1,9 +1,8 @@
 module "neo4j" {
-  count = var.create_db_instance ? 1: 0
   source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/neo4j"
   env = terraform.workspace
   vpc_id = var.vpc_id
-  db_subnet_id = var.db_subnet_id
+  db_subnet_id = var.private_subnet_ids[0]
   db_instance_volume_size = var.db_instance_volume_size
   public_ssh_key_ssm_parameter_name = var.public_ssh_key_ssm_parameter_name
   stack_name = var.stack_name
