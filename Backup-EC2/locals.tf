bbaf2e76732e1b91d17af1f4491f115d685259c1
Ngu <nguca@nci-02277953-ml.nih.gov>
2023-02-27 17:09:43 -0500

    updated

--- Backup-EC2/locals.tf
+++ Backup-EC2/locals.tf
@@ -12,5 +12,4 @@ locals {
   #nih_ip_cidrs =  terraform.workspace == "prod" || terraform.workspace == "stage" || var.cloud_platform == "leidos" ? ["0.0.0.0/0"]: [ "129.43.0.0/16" , "137.187.0.0/16"  , "165.112.0.0/16" , "156.40.0.0/16"  , "128.231.0.0/16" , "130.14.0.0/16" , "157.98.0.0/16"]
   nih_ip_cidrs  = ["0.0.0.0/0"]
   all_ips      =  var.cloud_platform == "leidos" ? ["0.0.0.0/0"] : local.nih_ip_cidrs
-  alb_subnet_ids = terraform.workspace == "prod" || terraform.workspace == "stage"  || var.cloud_platform == "leidos" ? var.public_subnet_ids : var.private_subnet_ids
 }
