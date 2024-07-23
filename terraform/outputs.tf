7af4291770a328bf9a2315c90bd2da8a5053b405
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2024-07-23 14:27:45 -0400

    updated outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,13 +1,3 @@
-//output "db_private_ip" {
-//  value = module.neo4j.private_ip
-//}
 output "opensearch_endpoint" {
-  value = module.opensearch.opensearch_endpoint
-}
-output "cluster_endpoint" {
-  value = module.aurora.*.cluster_endpoint
-}
-output "db_password" {
-  value     = module.aurora.*.db_password
-  sensitive = true
+  value = module.opensearch.endpoint
 }
