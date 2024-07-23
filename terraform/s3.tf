6f52860017415c7693c4c5d5267ef07e9e845667
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2024-07-23 14:40:35 -0400

    debug

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket_policy" "s3_snapshot_policy" {
   count  = terraform.workspace == "stage" ? 1 : 0
   bucket = module.s3_ossnapshot[0].bucket_id
-  policy = data.aws_iam_policy_document.opensearch_snapshot_policy[0].json
+  policy = data.aws_iam_policy_document.opensearch_snapshot_policy_document[0].json
 }
