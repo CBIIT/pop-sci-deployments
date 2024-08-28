7735a3da8921b03c888d42a730ad7e162b7d7170
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2024-08-28 16:06:59 -0400

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,8 +69,8 @@ module "opensearch" {
   automated_snapshot_start_hour = var.automated_snapshot_start_hour
   vpc_id                        = var.vpc_id
   create_cloudwatch_log_policy  = var.create_cloudwatch_log_policy
-  #create_snapshot_role          = var.create_snapshot_role
-  create_os_service_role        = var.create_os_service_role
+  create_snapshot_role          = var.create_snapshot_role
+  #create_os_service_role        = var.create_os_service_role
   resource_prefix               = "${var.program}-${terraform.workspace}-${var.project}"
 }
 
